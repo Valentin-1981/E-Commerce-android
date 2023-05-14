bad41f33b53ed7acab0c1e11c13f2996e751c58a
Valentine Chibinyov <chibinyov@rambler.ru>
2023-05-14 23:05:49 +0300

    fifth commit

--- app/src/main/java/com/testproject/e_commerce/MainActivity.java
+++ app/src/main/java/com/testproject/e_commerce/MainActivity.java
@@ -7,15 +7,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import com.testproject.e_commerce.adapter.CategoryAdapter;
+import com.testproject.e_commerce.adapter.CourseAdapter;
 import com.testproject.e_commerce.model.Category;
+import com.testproject.e_commerce.model.Course;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView categoryRecycler;
+    RecyclerView categoryRecycler, courseRecycler;
     CategoryAdapter categoryAdapter;
+
+    CourseAdapter courseAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +32,25 @@ public class MainActivity extends AppCompatActivity {
         categoryList.add(new Category(4, "Прочее"));
 
         setCategoryRecycler(categoryList);
+
+        List<Course> courseList = new ArrayList<>();
+        courseList.add(new Course(1, "java_2", "Профессия Java\nразработчик", "1 января", "начальный", "#424345"));
+        courseList.add(new Course(2, "python_3", "Профессия Python\nразработчик","10 января", "начальный", "#9FA52D"));
+//        courseList.add(new Category(3, "Языки"));
+//        courseList.add(new Category(4, "Прочее"));
+
+        setCourseRecycler(courseList);
+    }
+
+    private void setCourseRecycler(List<Course> courseList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        courseRecycler = findViewById(R.id.courseRecycler);
+        courseRecycler.setLayoutManager(layoutManager);
+
+        courseAdapter = new CourseAdapter(this, courseList);
+        courseRecycler.setAdapter(courseAdapter);
+
     }
 
     private void setCategoryRecycler(List<Category> categoryList) {
--- app/src/main/java/com/testproject/e_commerce/adapter/CourseAdapter.java
+++ app/src/main/java/com/testproject/e_commerce/adapter/CourseAdapter.java
@@ -1,31 +1,58 @@
 package com.testproject.e_commerce.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.testproject.e_commerce.R;
 import com.testproject.e_commerce.model.Course;
 
 import java.util.List;
 
-public class CourseAdapter extends RecyclerView.Adapter<> {
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseViewHolder> {
+
+    public CourseAdapter(Context context, List<Course> courses) {
+        this.context = context;
+        this.courses = courses;
+    }
 
     Context context;
     List<Course> courses;
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public CourseAdapter.CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View courseItems = LayoutInflater.from(context).inflate(R.layout.course_item, parent, false);
+        return new CourseAdapter.CourseViewHolder(courseItems);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CourseAdapter.CourseViewHolder holder, int position) {
+        holder.courseBg.setCardBackgroundColor(Color.parseColor(courses.get(position).getColor()));
 
+        int imageId = context.getResources().getIdentifier(courses.get(position).getImg(), "drawable", context.getPackageName());
+        holder.courseImage.setImageResource(imageId);
+
+        holder.courseDate.setText(courses.get(position).getDate());
+        holder.courseTitle.setText(courses.get(position).getTitle());
+        holder.courseLevel.setText(courses.get(position).getLevel());
     }
+
+
+//    @Override
+//    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+//
+//    }
 
     @Override
     public int getItemCount() {
@@ -34,8 +61,18 @@ public class CourseAdapter extends RecyclerView.Adapter<> {
 
     public static final class CourseViewHolder extends RecyclerView.ViewHolder {
 
+        CardView courseBg;
+        ImageView courseImage;
+        TextView courseTitle, courseDate, courseLevel;
+
         public CourseViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            courseBg = itemView.findViewById(R.id.courseBg);
+            courseImage = itemView.findViewById(R.id.courseImage);
+            courseTitle = itemView.findViewById(R.id.courseTitle);
+            courseDate = itemView.findViewById(R.id.courseDate);
+            courseLevel = itemView.findViewById(R.id.courseLevel);
         }
     }
 }
