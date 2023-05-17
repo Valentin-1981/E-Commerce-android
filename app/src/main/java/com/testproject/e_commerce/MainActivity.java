b6a0e64b180389e00788acb47273a671adf27a7e
Valentine Chibinyov <chibinyov@rambler.ru>
2023-05-17 17:12:51 +0300

    ninth commit

--- app/src/main/java/com/testproject/e_commerce/MainActivity.java
+++ app/src/main/java/com/testproject/e_commerce/MainActivity.java
@@ -19,7 +19,11 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView categoryRecycler, courseRecycler;
     CategoryAdapter categoryAdapter;
 
-    CourseAdapter courseAdapter;
+    static CourseAdapter courseAdapter;
+    static List<Course> courseList = new ArrayList<>();
+    static List<Course> fullCoursesList = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,15 +37,16 @@ public class MainActivity extends AppCompatActivity {
 
         setCategoryRecycler(categoryList);
 
-        List<Course> courseList = new ArrayList<>();
-        courseList.add(new Course(1, "java_2", "Профессия Java\nразработчик", "1 января", "начальный", "#424345", "Test"));
-        courseList.add(new Course(2, "python_3", "Профессия Python\nразработчик","10 января", "начальный", "#9FA52D", "Test"));
-        courseList.add(new Course(3, "cpp_2", "Профессия C++\nразработчик","15 июня", "начальный", "#E44C30", "Test"));
-        courseList.add(new Course(4, "unity_1", "Профессия Unity\nразработчик","02 июля", "начальный", "#4476D6", "Test"));
-        courseList.add(new Course(5, "csharp_1", "Профессия C#\nразработчик","10 июня", "начальный", "#0D0F29", "Test"));
-        courseList.add(new Course(6, "full_stack_1", "Профессия Full Stack\nразработчик","14 июня", "начальный", "#A2CAE3", "Test"));
+        courseList.add(new Course(1, "java_2", "Профессия Java\nразработчик", "1 января", "начальный", "#424345", "Test", 3));
+        courseList.add(new Course(2, "python_3", "Профессия Python\nразработчик","10 января", "начальный", "#9FA52D", "Test", 3));
+        courseList.add(new Course(3, "cpp_2", "Профессия C++\nразработчик","15 июня", "начальный", "#E44C30", "Test", 1));
+        courseList.add(new Course(4, "unity_1", "Профессия Unity\nразработчик","02 июля", "начальный", "#4476D6", "Test", 2));
+        courseList.add(new Course(5, "csharp_1", "Профессия C#\nразработчик","10 июня", "начальный", "#0D0F29", "Test", 2));
+        courseList.add(new Course(6, "full_stack_1", "Профессия Full Stack\nразработчик","14 июня", "начальный", "#A2CAE3", "Test", 1));
 //        courseList.add(new Category(3, "Языки"));
 //        courseList.add(new Category(4, "Прочее"));
+
+        fullCoursesList.addAll(courseList);
 
         setCourseRecycler(courseList);
     }
@@ -65,5 +70,23 @@ public class MainActivity extends AppCompatActivity {
 
         categoryAdapter = new CategoryAdapter(this, categoryList);
         categoryRecycler.setAdapter(categoryAdapter);
+    }
+
+    public static void showCoursesByCategory(int category){
+        courseList.clear();
+        courseList.addAll(fullCoursesList);
+
+        List<Course> filterCourses = new ArrayList<>();
+
+        for (Course c : courseList) {
+            if(c.getCategory() == category) {
+                filterCourses.add(c);
+            }
+        }
+
+        courseList.clear();
+        courseList.addAll(filterCourses);
+
+        courseAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/testproject/e_commerce/adapter/CategoryAdapter.java
+++ app/src/main/java/com/testproject/e_commerce/adapter/CategoryAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.testproject.e_commerce.MainActivity;
 import com.testproject.e_commerce.R;
 import com.testproject.e_commerce.model.Category;
 
@@ -34,6 +35,14 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     @Override
     public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
         holder.categoryTitle.setText(categories.get(position).getTitle());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                MainActivity.showCoursesByCategory(categories.get(position).getId());
+            }
+        });
     }
 
     @Override
